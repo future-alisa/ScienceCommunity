be7be526a96894abe9a6176a5f52680ce1689d4b
SunYubo <52072505+sunyuboalisa@users.noreply.github.com>
2024-09-11 17:42:03 +0800

    添加后端基本框架

---
+++ Server/science_community/src/test/java/com/alisa/science_community/ScienceCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alisa.science_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScienceCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
